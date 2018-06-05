87fb5d5cf44533b89a3589f9ec52a235f54bbb7a
erica mitchell <ericamitchell1221@gmail.com>
2018-06-04 21:37:26 -0400

    removed some files

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -1,30 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
-import ToDo from './components/ToDo.js';
+ import React, { Component } from 'react';
+ import './App.css';
+ import ToDo from './components/ToDo.js';
 
-class App extends Component {
-	constructor (props){
-		super(props);
-		this.state = {
-			todos:[
-				{description: 'Book venue', isComplete: true},
-					{description: 'Research tux rentals', isComplete: false},
-						{description: 'Contact Florist', isComplete: false}
-						]
-			};
-		}
-	   render() {
+ class App extends Component {
+   render() {
      return (
        <div className="App">
         <ul>
-		{ this.state.rodos.map (( todo, index) =>}
-          <ToDo key= { index } />
-          <ToDo  key = { index } description={ todo.description} isComplted ={ todo.isCompleted } />
-          )}
+          <ToDo />
+          <ToDo />
         </ul>       
        </div>
      );
    }
  }
 
-export default App;
+ export default App;
